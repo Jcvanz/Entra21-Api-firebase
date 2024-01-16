a3e9576f089af6333684169708f36599cc51a4b1
Jvanz <j.vanz1108@gmail.com>
2024-01-16 15:07:14 -0300

    Criado todas as rotas e atualizado o arq routes

--- src/Controllers/ProductControllers.js
+++ src/Controllers/ProductControllers.js
@@ -2,15 +2,83 @@ const db = require('../../firebaseConfig');
 
 // Config das rotas
 const ProductController = {
-    createProduto: async(req, res) => {
+    // Rota para criar produto
+    createProducts: async(req, res) => {
         try {
+            // Faz a requisição
             const produtoRef = db.collection('produtos').doc();
             await produtoRef.set(req.body);
+            // Resposta da requisição caso seja true
             res.status(201).json({ id: produtoRef.id, ...req.body });
         } catch(error) {
+            // Devolve o erro erro
             res.status(500).send(error.messagem);
         }
     },
+
+    // Rota para buscar todos os produtos
+    getAllProducts: async(req, res) => {
+        try {
+            // Faz a busca dos produtos
+            const produtoSnapshot = await db.collection('produtos').get();
+            const produtos = [];
+            produtoSnapshot.forEach(doc => {
+                produtos.push({ id:doc.id, ...doc.data() });
+            });
+            // Resposta da requisição caso seja true
+            res.status(200).json(produtos);
+        } catch(error) {
+            // Devolve o erro
+            res.status(500).send(error.messagem);
+        }
+    },
+
+    // Rota para buscar um produto pelo ID
+    getProdutoById: async(res, req) => {
+        try {
+            // Faz a busca do produto pelo ID
+            const produtoRef = db.collection('produtos').doc(req.params.id);
+            const doc = await produtoRef.get();
+            // Faz a verificação se existe o produto
+            if(!doc.exists) {
+                res.status(404).send('Produto não encontrado');
+            } else {
+                // Resposta da requisição caso seja true
+                res.status(200).json({ id: doc.id, ...doc.data() });
+            }
+        } catch(error) {
+            // Devolve o erro
+            res.status(500).send(error.messagem);
+        }
+    },
+
+    // Rota para buscar um produto pelo ID e atualizar ele
+    updateProducts: async(res, req) => {
+        try {
+            // Faz a busca do produto pelo ID e atualiza ele
+            const produtoRef = db.collection('produtos').doc(req.params.id);
+            await produtoRef.update(req.body);
+            // Resposta da requisição caso seja true
+            res.status(200).send('Produto atualizado com sucesso');
+        } catch(error) {
+            // Devolve o erro
+            res.status(500).send(error.messagem);
+        }
+    },
+    
+    // Rota para buscar um produto pelo ID e deletar ele
+    deleteProduto: async(res, req) => {
+        try {
+            // Faz a busca do produto pelo ID e deleta ele
+            const produtoRef = db.collection('produtos').doc(req.params.id);
+            await produtoRef.delete(req.body);
+            // Resposta da requisição caso seja true
+            res.status(200).send('Produto deletado com sucesso');
+        } catch(error) {
+            // Devolve o erro
+            res.status(500).send(error.messagem);
+        }
+    }
 }
 
 module.exports = ProductController;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const ProductController = require('../Controllers/ProductControllers');
 
 // Rota para criar um novo produto
-router.post('/produtos', ProductController.createProduto);
+router.post('/produtos', ProductController.createProducts);
 
 //Rota para obter um novo produto
-router.get('/produtos');
+router.get('/produtos', ProductController.getAllProducts);
 
 // Rota para obter um produto pelo ID
-router.get('/produtos/:id');
+router.get('/produtos/:id', ProductController.getProdutoById);
 
 // Rota para atualizar um produto
-router.put('/produtos/:id');
+router.put('/produtos/:id', ProductController.updateProducts);
 
 // Rota para deletar um produto
-router.delete('/produtos/:id');
+router.delete('/produtos/:id', ProductController.deleteProduto);
 
 module.exports = router;
